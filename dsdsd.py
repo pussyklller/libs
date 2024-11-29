f9eff51e8d337c428b4512862892ba7011692377
pussyklller <102186955+pussyklller@users.noreply.github.com>
2024-11-30 01:18:24 +0200

    Update dsdsd.py

--- dsdsd.py
+++ dsdsd.py
@@ -4,16 +4,7 @@ import platform
 import subprocess
 import sys
 import logging
-import importlib.metadata
 from .. import loader, utils
-
-
-def get_platform():
-    """Определение платформы и архитектуры."""
-    system = platform.system()
-    machine = platform.machine()
-    python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
-    return system, machine, python_version
 
 
 class TestLoadLIB(loader.Library):
